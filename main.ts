93a5876d03506d43bc6f110f58f03fbac0bfc4c9
Adam Kovar <kovaradam1@gmail.com>
2023-04-02 19:42:10 +0200

    specify lib versions

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { Application, Router } from "https://deno.land/x/oak@v12.1.0/mod.ts";
 import { oakCors } from "https://deno.land/x/cors@v1.2.2/mod.ts";
 import logger from "https://deno.land/x/oak_logger@1.0.0/mod.ts";
 
